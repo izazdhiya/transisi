338a1b4f24a0954f41e13455a656eebe590c60e4
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2024-02-21 14:25:46 +0700

    feat(php): transisi.php

---
+++ transisi.php
@@ -0,0 +1,97 @@
+<?php
+//  1: PHP DASAR
+
+$nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
+
+$arrayNilai = explode(" ", $nilai);
+
+$arrayNilai = array_map('intval', $arrayNilai);
+rsort($arrayNilai);
+
+$rataRata = array_sum($arrayNilai) / count($arrayNilai);
+echo "Rata-rata nilai: " . $rataRata . "<br>";
+
+$nilaiTertinggi = array_slice($arrayNilai, 0, 7);
+echo "7 Nilai Tertinggi: " . implode(", ", $nilaiTertinggi) . "<br>";
+
+$nilaiTerendah = array_slice($arrayNilai, -7, 7);
+echo "7 Nilai Terendah: " . implode(", ", $nilaiTerendah) . "<br>";
+
+// 2. PHP DASAR
+
+function jumlahHurufKecil($string) {
+    $jumlahHurufKecil = 0;
+
+    for ($i = 0; $i < strlen($string); $i++) {
+        if (ctype_lower($string[$i])) {
+            $jumlahHurufKecil++;
+        }
+    }
+
+    $output = '"' . $string . '" mengandung ' . $jumlahHurufKecil . ' buah huruf kecil.';
+    return $output;
+}
+
+$inputString = "TranSISI";
+$output = jumlahHurufKecil($inputString);
+echo $output;
+
+
+// 3. PHP DASAR
+
+function generateNGrams($text, $n)
+{
+    $tokens = explode(' ', $text);
+
+    $ngrams = [];
+    for ($i = 0; $i < count($tokens) - $n + 1; $i++) {
+        $ngram = implode(' ', array_slice($tokens, $i, $n));
+        $ngrams[] = $ngram;
+    }
+
+    return $ngrams;
+}
+
+$string = "Jakarta adalah ibukota negara Republik Indonesia";
+
+$unigrams = generateNGrams($string, 1);
+echo "Unigram: " . implode(', ', $unigrams) . "\n";
+
+$bigrams = generateNGrams($string, 2);
+echo "Bigram: " . implode(', ', $bigrams) . "\n";
+
+$trigrams = generateNGrams($string, 3);
+echo "Trigram: " . implode(', ', $trigrams) . "\n";
+
+// 4. PHP DASAR
+
+
+
+// 5. PHP DASAR
+
+function encryptString($input)
+{
+    $output = '';
+
+    for ($i = 0; $i < strlen($input); $i++) {
+        $char = $input[$i];
+
+        if ($char >= 'A' && $char <= 'Z') {
+            $encryptedChar = chr(((ord($char) - 65 + 1) % 26) + 65);
+            $output .= $encryptedChar;
+        } elseif ($char >= 'a' && $char <= 'z') {
+            $encryptedChar = chr(((ord($char) - 97 + 1) % 26) + 97);
+            $output .= $encryptedChar;
+        } else {
+            $output .= $char;
+        }
+    }
+
+    return $output;
+}
+
+$inputString = "DFHKNQ";
+$encryptedString = encryptString($inputString);
+
+echo "Input: $inputString\n";
+echo "Encrypted Output: $encryptedString\n";
